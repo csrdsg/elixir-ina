7e6c1c53da653fa4275a330c708e0cf24ee16f8f
csrdsg <csrds.g@gmail.com>
2022-05-17 14:00:50 +0200

    base questin funcs

--- mastery/lib/mastery/core/question.ex
+++ mastery/lib/mastery/core/question.ex
@@ -1,3 +1,36 @@
 defmodule Master.Core.Question do
-  defstruct ~w[asked subtitutions templates]a
+
+defstruct ~w[asked subtitutions templates]a
+
+def new(%Template{ } = template) do
+  template.generators
+  |> Enum.map(&build_subtitution/1)
+  |> evaulate(template)
+end
+
+defp build_subtitution({name, choices_or_generator}) do
+  {name, choose(choices_or_generator)}
+end
+
+defp choose(choices) when is_list(choices) do
+  Enum.random(choices)
+end
+
+defp choose(generator) when is_function(generator) do
+  generator.()
+end
+
+defp compile(template, subtitutions) do
+  template.compiled
+  |> Code.eval_quoted(assigns: subtitutions)
+  |> elem(0)
+end
+
+defp evaulate(subtitutions, template) do
+  %__MODULE__{
+    asked: compile(template, subtitutions),
+    subtitutions: subtitutions,
+    template: template
+  }
+end
 end
