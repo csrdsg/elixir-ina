60b0eaf9b38c5c3ce6798df8898e31a7de16e8ad
csrdsg <csrds.g@gmail.com>
2022-05-18 15:53:02 +0200

    some formatting and checking

--- mastery/lib/mastery/core/response.ex
+++ mastery/lib/mastery/core/response.ex
@@ -1,18 +1,18 @@
 defmodule Mastery.Core.Response do
   defstruct ~w[quiz_title template_name to email answer correct timestamp]a
 
-def new(quiz, email, answer) do
-  question = quiz.current_question
-  template = question.template
+  def new(quiz, email, answer) do
+    question = quiz.current_question
+    template = question.template
 
-  %__MODULE__{
-    quiz_title: quiz.title,
-    template_name: template.name,
-    to: question.asked,
-    email: email,
-    answer: answer,
-    correct: template.checker(question.subtitutions, answer),
-    timestamp: DateTime.utc_now()
-  }
-end
+    %__MODULE__{
+      quiz_title: quiz.title,
+      template_name: template.name,
+      to: question.asked,
+      email: email,
+      answer: answer,
+      correct: template.checker(question.subtitutions, answer),
+      timestamp: DateTime.utc_now()
+    }
+  end
 end
